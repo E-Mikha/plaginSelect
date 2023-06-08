aef19f7cb8d81380a47ecdf01aeef6227b46e007
E-Mikha <134232916+E-Mikha@users.noreply.github.com>
2023-06-09 02:42:15 +0700

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  devtool: "source-map",
+  watch: true,
+  entry: {
+    filename: "./app.js",
+  },
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
